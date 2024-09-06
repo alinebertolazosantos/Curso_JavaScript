2dafd81ccd6610034eaf2225d20e84d08f47db03
Aline Bertolazo dos Santos <aline16bertolazo04@gmail.com>
2024-09-05 23:32:15 -0300

    atualizando exercicio 5 da lista 4

--- ListaExercicios3/exercicio5.js
+++ ListaExercicios3/exercicio5.js
@@ -1,22 +1,3 @@
-// Exercício 5: Manipulação dos Tipos Referências
-// a. Crie um array estoque e atribua seus valores para um novo array novoEstoque.
-// b. Modifique um elemento de novoEstoque e mostre que estoque não foi alterado.
-// c. Crie um objeto funcionario e atribua seus valores para um novo objeto
-// novoFuncionario.
-// d. Modifique uma propriedade de novoFuncionario e mostre que funcionario não foi
-// alterado.
-
-let estoque = [10, 20, 30, 40, 50];
-
-let novoEstoque = [...estoque];
-
-console.log(estoque);
-
-novoEstoque[1] = 30;
-
-console.log('estoque ' + estoque);
-console.log('novo estoque ' + novoEstoque);
-
 let funcionario = {
     nome: 'Aline',
     idade: 22,
